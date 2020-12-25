b42fdba74b3bb1396441f73a80ba5a93196477f9
Prabakaran Kuppusamy <prabakaran.kuppusamy@mrcooper.com>
2020-12-25 21:00:27 +0530

    fix incorrect answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,15 +10,11 @@ class QuizQuestion extends Component {
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
-            this.setState( state => {
-                return {incorrectAnswer: true}
-            });
+            this.setState({incorrectAnswer: false});
             this.props.showNextQuestionHandler()
         }
         else {
-            this.setState( state => {
-                return {incorrectAnswer: false}
-            });
+            this.setState({incorrectAnswer: true});
         }
     }
 
@@ -39,6 +35,7 @@ class QuizQuestion extends Component {
             })}
         </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
       )
     }
